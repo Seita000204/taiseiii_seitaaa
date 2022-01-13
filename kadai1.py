cb815b23a1562bc52047818983b2a01de3125fe8
Seita000204 <gp19a060@oecu.jp>
2022-01-14 03:17:38 +0900

    [fix] 100回から200回に変更

--- kadai1.py
+++ kadai1.py
@@ -1,4 +1,4 @@
-for num in range(1,101):
+for num in range(1,201):
     if num % 15 == 0:
         print("だいがく爆発したい")
     elif num % 3 == 0:
@@ -7,4 +7,3 @@ for num in range(1,101):
         print("就活したい")
     else:
         print(num)
-        
